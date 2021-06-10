3f44f57d6c5f93d1260ab2d5238df1a6510fd959
René <snooz@posteo.de>
2021-06-10 19:11:58 +0200

    added cookbook

--- RobotFrameworkTest/console.sh
+++ RobotFrameworkTest/console.sh
@@ -1,9 +1,6 @@
 curl https://data.imbus.de/index.php/s/mrKfokmz79apCBd/download --output intelli.zip
 unzip intelli.zip -d /opt/.katacodacode/extensions/snooz82.rf-intellisense-2.9.6
 mv /opt/.katacodacode/extensions/snooz82.rf-intellisense-2.9.6/extension/* /opt/.katacodacode/extensions/snooz82.rf-intellisense-2.9.6/
-pip install robotframework
-pip install robotframework-stacktrace
-pip install robotframework-crypto
-pip install robotframework-pabot
-git clone https://github.com/robotframework/robotframework.git
 rm intelli.zip
+git clone https://github.com/adrianyorke/robotframework-cookbook.git
+
---
+++ RobotFrameworkTest/script.sh
@@ -0,0 +1,6 @@
+pip install robotframework
+pip install robotframework-stacktrace
+pip install robotframework-datadriver
+pip install robotframework-crypto
+pip install robotframework-pabot
+pip install xmlschema
