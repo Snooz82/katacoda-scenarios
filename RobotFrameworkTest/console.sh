bc1d62bb021cebd40adfd4fa671dc3673ff33fa3
René <snooz@posteo.de>
2021-06-10 20:59:01 +0200

    added own repo

--- RobotFrameworkTest/console.sh
+++ RobotFrameworkTest/console.sh
@@ -2,5 +2,5 @@ curl https://data.imbus.de/index.php/s/mrKfokmz79apCBd/download --output intelli
 unzip intelli.zip -d /opt/.katacodacode/extensions/snooz82.rf-intellisense-2.9.6
 mv /opt/.katacodacode/extensions/snooz82.rf-intellisense-2.9.6/extension/* /opt/.katacodacode/extensions/snooz82.rf-intellisense-2.9.6/
 rm intelli.zip
-git clone https://github.com/adrianyorke/robotframework-cookbook.git
+git clone git@github.com:Snooz82/robotframework-training.git
 
