7071c8d80fdf788422166ad517f402ba43808dd7
René <snooz@posteo.de>
2021-06-10 17:21:38 +0200

    fix

---
+++ RobotFrameworkTest/console.sh
@@ -0,0 +1,5 @@
+curl https://data.imbus.de/index.php/s/mrKfokmz79apCBd/download --output intelli.vsix
+pip install robotframework
+pip install robotframework-stacktrace
+pip install robotframework-crypto
+pip install robotframework-pabot
