586a21e84a4ff9fb7b6d9a3d6779b15b54dae199
René <snooz@posteo.de>
2021-06-10 18:35:41 +0200

    test

--- RobotFrameworkTest/console.sh
+++ RobotFrameworkTest/console.sh
@@ -1,5 +1,4 @@
-curl https://data.imbus.de/index.php/s/mrKfokmz79apCBd/download --output intelli.vsix
-mv intelli.vsix intelli.zip
+curl https://data.imbus.de/index.php/s/mrKfokmz79apCBd/download --output intelli.zip
 unzip intelli.zip -d /opt/.katacodacode/extensions/snooz82.rf-intellisense-2.9.6
 mv /opt/.katacodacode/extensions/snooz82.rf-intellisense-2.9.6/extensions/*.* /opt/.katacodacode/extensions/snooz82.rf-intellisense-2.9.6/
 pip install robotframework
