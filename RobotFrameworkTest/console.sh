c4fa7c6f4e3dc09f8512d2b34380acc258a8ed0b
René <snooz@posteo.de>
2021-06-10 18:53:25 +0200

    test

--- RobotFrameworkTest/console.sh
+++ RobotFrameworkTest/console.sh
@@ -1,8 +1,8 @@
 curl https://data.imbus.de/index.php/s/mrKfokmz79apCBd/download --output intelli.zip
+unzip intelli.zip -d /opt/.katacodacode/extensions/snooz82.rf-intellisense-2.9.6
 mv /opt/.katacodacode/extensions/snooz82.rf-intellisense-2.9.6/extension/* /opt/.katacodacode/extensions/snooz82.rf-intellisense-2.9.6/
 pip install robotframework
 pip install robotframework-stacktrace
 pip install robotframework-crypto
 pip install robotframework-pabot
-unzip intelli.zip -d /opt/.katacodacode/extensions/snooz82.rf-intellisense-2.9.6
 git clone https://github.com/adiralashiva8/robotframework-examples.git .
